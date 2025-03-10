5409d27f1cc0c857400edb424ccbec0216dd79ff
Your Name <your-email@example.com>
2025-03-10 15:48:32 +0530

    done

---
+++ 12_Sets.py
@@ -0,0 +1,12 @@
+arr=[1,3,4,2,1,5,4,6,8]
+sets = set(arr)
+print(sets)
+arr = list(sets)
+print(arr)
+print(f"Smallest Num of the array in {arr[0]}")
+s={8,2,8,4,0}
+print(s)
+arr=list(s)
+print(arr)
+arr.sort()
+print(arr)
